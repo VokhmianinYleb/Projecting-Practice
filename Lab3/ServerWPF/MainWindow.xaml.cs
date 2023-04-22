8c62370632f2b8e1cef678eb93a20fce846fc111
PandeMiaa <80745115+VokhmianinYleb@users.noreply.github.com>
2023-04-22 22:50:03 +0300

    Laboratorna 3

---
+++ Lab3/ServerWPF/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿// MainWindow.xaml.cs
+
+using System.Windows;
+
+namespace ServerWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Обробник подій при натисканні на пункт меню
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            // Створення нового вікна з усіма номерами готелю
+            Window window = new HotelRooms();
+            // Демонстрація вікна
+            window.Show();
+        }
+    }
+}
